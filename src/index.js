daf51809872a04fbb746ad7d05a0cb494b3f5de9
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2025-02-10 23:41:25 -0800

    Make the buttons at the top work to switch between tabbed pages.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import './main.css';
 import { Display_HomePage } from './homepage';
 import { Display_MenuPage } from './menu';
 
-//Display_HomePage();
-Display_MenuPage();
+Display_HomePage();
+
+
+const Clear_DOM = () => {
+    let content = document.querySelector('#content');
+    content.innerHTML = "";
+}
+
+let home_button = document.querySelector('#home');
+home_button.addEventListener('click', function(){
+    Clear_DOM();
+    Display_HomePage();
+});
+
+let menu_button = document.querySelector('#menu');
+menu_button.addEventListener('click', function(){
+    Clear_DOM();
+    Display_MenuPage();
+});
